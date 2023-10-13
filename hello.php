d00cc6dadce718a65fe454fa06e62881bd0c45f0
Abbas Ali <abbas@ranium.in>
2023-10-13 14:05:49 +0530

    Initial commit upto forms

---
+++ hello.php
@@ -0,0 +1,28 @@
+<?php
+
+echo 'hello world';
+
+$integer = 345;
+$string = 'hello world';
+$boolean = true;
+$float = 45.34;
+
+
+//$names = [
+//    'name' => 'priya',
+//    'age' => 20,
+//    'percentage' => 95.98,
+//    'localite' => true,
+//];
+//
+//include 'hello.template.php';
+
+
+$something = 'false';
+
+if ($something) {
+    echo  'yes';
+} else {
+    echo 'no';
+}
+?>
---
+++ hello.template.php
@@ -0,0 +1,6 @@
+<ul>
+    <li>Name: <?= $names['name'] ?></li>
+    <li>Age: <?= $names['age'] ?></li>
+    <li>Percentage: <?= $names['percentage'] ?></li>
+    <li>Localite: <?= $names['localite'] ? 'yes' : 'no' ?></li>
+</ul>
