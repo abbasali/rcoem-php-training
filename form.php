abb7774513cc55512a62d7ac838aefa6a587080d
Abbas Ali <abbas@ranium.in>
2023-10-13 18:24:33 +0530

    Forms done.

--- form.php
+++ form.php
@@ -1,4 +1,21 @@
 <?php
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+function dump($value) {
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+}
+?>
+<div>My name is <?= $_POST['name'] ?>. My favorite color is 
+<?php if ($_POST['gender'] == 'male'): ?>
+    blue.
+<?php else: ?>
+    pink.
+<?php endif; ?>
+    <?php if (isset($_POST['subjects'])): ?>
+        My favorite subjects are: 
+        <?= implode(', ', $_POST['subjects']); ?>.
+    <?php endif; ?>
+    I live in <?= $_POST['city']; ?>.
+</div>
+
+<?php dump($_POST); ?>
