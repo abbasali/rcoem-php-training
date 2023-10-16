8b14ff28d48e119f6ff09a661df505e23d841814
Abbas Ali <abbas@ranium.in>
2023-10-16 10:03:10 +0530

    Class (OOPs) basics

---
+++ oops.php
@@ -0,0 +1,26 @@
+<?php
+include 'functions.php';
+class Calculator {
+    public int $value1;
+    public int $value2;
+    function __construct($value1, $value2) {
+        $this->value1 = $value1;
+        $this->value2 = $value2;
+    }
+    
+    function sum(): int
+    {
+        return $this->value1 + $this->value2;
+    }
+
+    function multiply(): int
+    {
+        return $this->value1 * $this->value2;
+    }
+}
+
+$calcutor = new Calculator(45, 10);
+
+echo $calcutor->sum();
+echo '<br />';
+echo $calcutor->multiply();
